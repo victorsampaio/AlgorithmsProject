1a7b6abdf007a070d91b3efd9c32f7c0dfe4e84b
Victor Fernando de Oliveira Sampaio <victorfsampaio@gmail.com>
2014-11-16 19:50:14 -0300

    Android: Algorithm - INSS #1.3
    Corrections

--- app/src/main/java/android/com/algorithmsproject/inss/InssSolucaoActivity.java
+++ app/src/main/java/android/com/algorithmsproject/inss/InssSolucaoActivity.java
@@ -44,10 +44,14 @@ public class InssSolucaoActivity extends Activity implements OnClickListener{
             // Valor Sal. Liq.
             TextView infoSalarioLiq = (TextView)findViewById(R.id.edtSalarioLiq);
             infoSalarioLiq.setText("Salario Liquido: " + valorSalario);
+
+            Toast.makeText(this, "ISENTO", Toast.LENGTH_SHORT).show();
         }
        else  if (valorSalario > 800 && valorSalario <= 1200) {
 
-
+            float valSalBruto = valorSalario;
+            float valDesc = (valSalBruto * 20) / 100;
+            float valSalLiq = valSalBruto - valDesc;
 
 
             // Salario Bruto
@@ -56,19 +60,52 @@ public class InssSolucaoActivity extends Activity implements OnClickListener{
 
             // Desnconto INSS
             TextView classificDesc = (TextView)findViewById(R.id.edtClassificaDesc);
-            classificDesc.setText("Classificação do Desconto: ISENTO");
+            classificDesc.setText("Classificação do Desconto: " + valDesc);
 
             // Valor Sal. Liq.
             TextView infoSalarioLiq = (TextView)findViewById(R.id.edtSalarioLiq);
-            infoSalarioLiq.setText("Salario Liquido: " + valorSalario);
+            infoSalarioLiq.setText("Salario Liquido: " + valSalLiq);
 
+            Toast.makeText(this, "DESCONTO DE 20%", Toast.LENGTH_SHORT).show();
 
         } else  if (valorSalario > 1200 && valorSalario <= 2000){
+            float valSalBruto = valorSalario;
+            float valDesc = (valSalBruto * 25) / 100;
+            float valSalLiq = valSalBruto - valDesc;
 
-        } else if (valorSalario <2000){
 
-        }else {
-            Toast.makeText(this, "Verifique o valor", Toast.LENGTH_SHORT).show();
+            // Salario Bruto
+            TextView infoSalarioBruto = (TextView) findViewById(R.id.edtSalarioBruto);
+            infoSalarioBruto.setText("Salario Bruto: " + valorSalario);
+
+            // Desnconto INSS
+            TextView classificDesc = (TextView)findViewById(R.id.edtClassificaDesc);
+            classificDesc.setText("Classificação do Desconto: " + valDesc);
+
+            // Valor Sal. Liq.
+            TextView infoSalarioLiq = (TextView)findViewById(R.id.edtSalarioLiq);
+            infoSalarioLiq.setText("Salario Liquido: " + valSalLiq);
+
+            Toast.makeText(this, "DESCONTO DE 25%", Toast.LENGTH_SHORT).show();
+
+        } else if (valorSalario > 2000){
+            float valSalBruto = valorSalario;
+            float valDesc = (valSalBruto * 30) / 100;
+            float valSalLiq = valSalBruto - valDesc;
+
+            // Salario Bruto
+            TextView infoSalarioBruto = (TextView) findViewById(R.id.edtSalarioBruto);
+            infoSalarioBruto.setText("Salario Bruto: " + valorSalario);
+
+            // Desnconto INSS
+            TextView classificDesc = (TextView)findViewById(R.id.edtClassificaDesc);
+            classificDesc.setText("Classificação do Desconto: " + valDesc);
+
+            // Valor Sal. Liq.
+            TextView infoSalarioLiq = (TextView)findViewById(R.id.edtSalarioLiq);
+            infoSalarioLiq.setText("Salario Liquido: " + valSalLiq);
+
+            Toast.makeText(this, "DESCONTO DE 30%", Toast.LENGTH_SHORT).show();
         }
     }
 
